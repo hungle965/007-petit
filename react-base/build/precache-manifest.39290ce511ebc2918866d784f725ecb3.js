6fdd04ddf9f1d554c0aae738f153d99afc226fc9
root <root@li1761-23.members.linode.com>
2020-08-21 01:41:02 +0000

    react-base

---
+++ react-base/build/precache-manifest.39290ce511ebc2918866d784f725ecb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "757425b1a23de0e72112cd27cbd396ab",
+    "url": "/template/react-base/build/index.html"
+  },
+  {
+    "revision": "58bbc69a6f3fc95940bb",
+    "url": "/template/react-base/build/static/css/main.417b99dc.chunk.css"
+  },
+  {
+    "revision": "399b9ee1d3ea1f520910",
+    "url": "/template/react-base/build/static/js/2.0e4e23c3.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/template/react-base/build/static/js/2.0e4e23c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58bbc69a6f3fc95940bb",
+    "url": "/template/react-base/build/static/js/main.fe82de1e.chunk.js"
+  },
+  {
+    "revision": "335227595cae28652598",
+    "url": "/template/react-base/build/static/js/runtime-main.da55bb54.js"
+  }
+]);
